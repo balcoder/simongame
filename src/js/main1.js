dd8dfc9ba7fb9cd2f6dc89d75011fa6db41917ce
Des Barrett <des.barrett@gmail.com>
2017-10-07 14:43:15 +0100

    fixed simonplay fn

--- src/js/main1.js
+++ src/js/main1.js
@@ -2,57 +2,108 @@
   "use strict";
 
   document.addEventListener('DOMContentLoaded', function() {
-    // var myAudio = document.getElementById('song');
-    // document.addEventListener('click', play);
-    // function play(){
-    //   myAudio.play();
-    // }
-    var blueButton = document.getElementById('lcornerst');
-    var blueSound = document.getElementById('audioBlue');
-     blueButton.addEventListener("click",playBlue);
-    function playBlue() {
-      blueSound.play();
-    }
-    var redButton = document.getElementById('rcornerst');
-    var redSound = document.getElementById('audioRed');
-    redButton.addEventListener("click",playRed);
-    function playRed() {
-      redSound.play();
-    }
-    var greenButton = document.getElementById('lcornersb');
-    var greenSound = document.getElementById('audioGreen');
-    greenButton.addEventListener("click",playGreen);
-    function playGreen() {
-      greenSound.play();
-    }
-    var yellowButton = document.getElementById('rcornersb');
-    var yellowSound = document.getElementById('audioYellow');
-    yellowButton.addEventListener("click",playYellow);
-    function playYellow() {
-      yellowSound.play();
-    }
-
-  //  var isPlaying = false;
-   //
-  //  function togglePlay() {
-  //      if (isPlaying) {
-  //          myAudio.pause();
-  //      } else {
-  //          myAudio.play();
-  //      }
-  //  };
-  //  myAudio.onplaying = function() {
-  //      isPlaying = true;
-  //  };
-  //  myAudio.onpause = function() {
-  //      isPlaying = false;
-  //  };
-
+    //set up variables
+    var count = 0;
+    var simonCount = 0;
+    var reset = 0;
+    var simonArr = [];
+    var playerArr = [];
+    var blueButton = document.getElementById('blue');
+    var redButton = document.getElementById('red');
+    var greenButton = document.getElementById('green');
+    var yellowButton = document.getElementById('yellow');
+    var buttonIds = [blueButton, redButton, greenButton, yellowButton];
     // create array of sounds
     var soundArray = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
     'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
     'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
     'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'];
 
+
+    var blueSound = document.getElementById('audioBlue');
+     blueButton.addEventListener("click",playBlue);
+    function playBlue() {
+      blueSound.play();
+    }
+
+    var redSound = document.getElementById('audioRed');
+    redButton.addEventListener("click",playRed);
+    function playRed() {
+      redSound.play();
+    }
+
+    var greenSound = document.getElementById('audioGreen');
+    greenButton.addEventListener("click",playGreen);
+    function playGreen() {
+      greenSound.play();
+    }
+
+    var yellowSound = document.getElementById('audioYellow');
+    yellowButton.addEventListener("click",playYellow);
+    function playYellow() {
+      yellowSound.play();
+    }
+    var startBtn = document.getElementById('start');
+    startBtn.addEventListener("click", start);
+
+    function start() {
+      count = 0;
+      simonCount = 0;
+      simonArr = [];
+      function simonPlay() {
+        var randomBtnId = buttonIds[Math.floor(Math.random()*buttonIds.length)];
+        simonArr.push(randomBtnId);
+        var countUp = 0;
+        setInterval(function(){
+          if(countUp < simonArr.length){
+            simonPress(simonArr[countUp]);
+            console.log(countUp);
+            countUp++;
+          } else
+              return;
+        }, 1000);
+        // simonArr.forEach(function(id){
+        //   setInterval(simonPress(id), 1000);
+        // });
+      }
+      function simonPress(id) {
+          var intervId;
+          ++simonCount;
+
+          switch(id){
+            case blueButton:
+              playBlue();
+              break;
+            case redButton:
+              playRed();
+              break;
+            case greenButton:
+              playGreen();
+              break;
+            case yellowButton:
+              playYellow();
+              break;
+          }
+      }
+
+      while(count < 20 && reset === 0){
+        //play the game
+        simonPlay(buttonIds);
+        ++count;
+        //playerPlay();
+      }
+    }
+
+    function playerPlay(){
+
+
+
+    }
+    function changeColor() {
+      intervId = setInterval(flashBtn, 1000);
+    }
+    function flashBtn(id) {
+      id.id = 'redSimon';
+    }
 
   });
---
+++ src/js/main3.js
@@ -0,0 +1,107 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function() {
+  //set up variables
+
+  var simonCount = 0;
+  var reset = 0;
+  var simonArr = [];
+  var playerArr = [];
+  var blueButton = document.getElementById('blue');
+  var redButton = document.getElementById('red');
+  var greenButton = document.getElementById('green');
+  var yellowButton = document.getElementById('yellow');
+  var buttonIds = [blueButton, redButton, greenButton, yellowButton];
+  // create array of sounds
+  var soundArray = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
+  'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
+  'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
+  'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'];
+  // get sound id and add click event
+  var blueSound = document.getElementById('audioBlue');
+   blueButton.addEventListener("click",playBlue);
+  function playBlue() {
+    blueSound.play();
+  }
+
+  var redSound = document.getElementById('audioRed');
+  redButton.addEventListener("click",playRed);
+  function playRed() {
+    redSound.play();
+  }
+
+  var greenSound = document.getElementById('audioGreen');
+  greenButton.addEventListener("click",playGreen);
+  function playGreen() {
+    greenSound.play();
+  }
+
+  var yellowSound = document.getElementById('audioYellow');
+  yellowButton.addEventListener("click",playYellow);
+  function playYellow() {
+    yellowSound.play();
+  }
+  // add click event for start buttonIdsvar startBtn = document.getElementById('start');
+  var startBtn = document.getElementById('start');
+  startBtn.addEventListener("click", start);
+
+  //put 20 random ids  into simonArr for simon to play
+  // for (var i = 0; i < 20; i++) {
+  //   var randomBtnId = buttonIds[Math.floor(Math.random()*buttonIds.length)];
+  //   simonArr.push(randomBtnId);
+  // }
+  function start() {
+    var count = 0;
+    simonArr = [];
+    function simonPlay() {
+      var randomBtnId = buttonIds[Math.floor(Math.random()*buttonIds.length)];
+      simonArr.push(randomBtnId);
+      count++;
+      var interval = 1000; //  = 2s
+      var increment = 1;
+      simonArr.forEach(function(id){
+        var runner = setTimeout(function(){
+          simonPress(id);
+          clearTimeout(runner);
+        },  interval * increment++);
+      });
+      // setInterval(function(){
+      //   if(countUp <= count){
+      //     simonPress(simonArr[countUp]);
+      //     console.log(simonArr[countUp]);
+      //     countUp++;
+      //   } else
+      //       return;
+      // }, 1000);
+    }
+    function simonPress(id) {
+        var intervId;
+        ++simonCount;
+
+        switch(id){
+          case blueButton:
+            playBlue();
+            break;
+          case redButton:
+            playRed();
+            break;
+          case greenButton:
+            playGreen();
+            break;
+          case yellowButton:
+            playYellow();
+            break;
+        }
+    }
+
+    while(count < 20 && reset === 0){
+      //play the game
+      simonPlay();
+      ++count;
+      //playerPlay();
+    }
+  }
+
+
+
+});
