83ce8942d90f6bbf887d72dc04b6ab65cabfc190
Des Barrett <des.barrett@gmail.com>
2017-10-09 22:29:36 +0100

    added checkSequence()

--- src/js/main4.js
+++ src/js/main4.js
@@ -1,26 +1,31 @@
+// When start button is pressed call simonPlay() to add a random button id to
+// the simon array and play that sequence with timeouts
+// When button is clicked play the audio, increment the counter and check the
+// that the sequence matches the simon array
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function() {
   //set up variables
-
-  //var simonCount = 0;
   var reset = 0;
   var simonArr = [];
   var playerArr = [];
   var playerCnt = 0;
+  // get the id's of all the buttons
   var blueButton = document.getElementById('blue');
   var redButton = document.getElementById('red');
   var greenButton = document.getElementById('green');
   var yellowButton = document.getElementById('yellow');
   var buttonIds = [blueButton, redButton, greenButton, yellowButton];
-  // create array of sounds
-  var soundArray = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
-  'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
-  'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
-  'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'];
-  // get sound id and add click event
+  // get the id's of all the audio
   var blueSound = document.getElementById('audioBlue');
-   blueButton.addEventListener("click",playBlue);
+  var redSound = document.getElementById('audioRed');
+  var greenSound = document.getElementById('audioGreen');
+  var yellowSound = document.getElementById('audioYellow');
+  // add click event for start button
+  var startBtn = document.getElementById('start');
+  startBtn.addEventListener("click", start);
+
+  blueButton.addEventListener("click",playBlue);
   function playBlue() {
     blueSound.play();
     playerArr.push(blueButton);
@@ -28,49 +33,65 @@ document.addEventListener('DOMContentLoaded', function() {
     checkSeq(blueButton);
   }
 
-  var redSound = document.getElementById('audioRed');
   redButton.addEventListener("click",playRed);
   function playRed() {
     redSound.play();
-    //console.log(this.id);
-    var kkk = this.id;
+    playerArr.push(redButton);
+    playerCnt++;
+    checkSeq(redButton);
   }
 
-  var greenSound = document.getElementById('audioGreen');
   greenButton.addEventListener("click",playGreen);
   function playGreen() {
     greenSound.play();
+    playerArr.push(greenButton);
+    playerCnt++;
+    checkSeq(greenButton);
   }
 
-  var yellowSound = document.getElementById('audioYellow');
   yellowButton.addEventListener("click",playYellow);
   function playYellow() {
     yellowSound.play();
+    playerArr.push(yellowButton);
+    playerCnt++;
+    checkSeq(yellowButton);
   }
-  // add click event for start buttonIdsvar startBtn = document.getElementById('start');
-  var startBtn = document.getElementById('start');
-  startBtn.addEventListener("click", start);
 
   // check for correct sequence of presses
   function checkSeq(btnPressed){
     //compare sequence of this buttonclick id with simonArr
     //simonArr[0].id
-    if(playerCnt > 0 && btnPressed === simonArr[playerCnt - 1]){
-
+    // if(btnPressed != simonArr[playerCnt - 1]){
+    if(comparePressed(btnPressed) && playerArr.length !== simonArr.length){
+        //do nothing
+    } else if (!comparePressed(btnPressed)) {
+      simonReplay();
+    } else {
+      simonPlay();
     }
   }
 
-  function start() { // when start button pressed
-    var count = 0;
-    simonArr = []; // reset array
-
-    while(count < 2 ){
-      //play the game
-      simonPlay();
-      ++count;
-      console.log("Great it works");
-      //playerPlay();
+  function comparePressed(btnPressed) {
+    var i = simonArr.length;
+    if (simonArr[playerCnt-1] === btnPressed){
+      return true;
+    } else{
+      return false;
     }
+  }
+
+
+
+
+  // when start button pressed
+  function start() {
+    var count = 0;
+    playerCnt = 0;
+    simonArr = []; // reset array
+    simonPlay();
+    ++count;
+    }
+
     // gets random button, adds to array and sets timeout for simonPress()
     function simonPlay() {
       var randomBtnId = buttonIds[Math.floor(Math.random() * buttonIds.length)];
@@ -86,35 +107,47 @@ document.addEventListener('DOMContentLoaded', function() {
           //console.log(increment+" "+ saveMe);
         },  interval * increment++);
       });
-      // setInterval(function(){
-      //   if(countUp <= count){
-      //     simonPress(simonArr[countUp]);
-      //     console.log(simonArr[countUp]);
-      //     countUp++;
-      //   } else
-      //       return;
-      // }, 1000);
+
+    }
+    // replay the sequence when player gets it wrong
+    function simonReplay() {
+      playerCnt = 0;
+      playerArr = [];
+      var interval = 1200;
+      var increment = 1;
+      simonArr.forEach(function(id){
+        //var saveMe = increment;
+        var runner = setTimeout(function(){
+          simonPress(id);
+          clearTimeout(runner);
+          //console.log(increment+" "+ saveMe);
+        },  interval * increment++);
+      });
     }
     // takes an id, plays sound and flashes button
     function simonPress(id) {
         switch(id){
           case blueButton:
-            playBlue();
+            //playBlue();
+            blueSound.play();
             id.className = "col-xs-6 gameBtn blueLight";
             setTimeout(revertButton, 1000, "col-xs-6 gameBtn blue");
             break;
           case redButton:
-            playRed();
+            //playRed();
+            redSound.play();
             id.className = "col-xs-6 gameBtn redLight";
             setTimeout(revertButton, 1000, "col-xs-6 gameBtn red");
             break;
           case greenButton:
-            playGreen();
+            //playGreen();
+            greenSound.play();
             id.className = "col-xs-6 gameBtn greenLight";
             setTimeout(revertButton, 1000, "col-xs-6 gameBtn green");
             break;
           case yellowButton:
-            playYellow();
+            //playYellow();
+            yellowSound.play();
             id.className = "col-xs-6 gameBtn yellowLight";
             setTimeout(revertButton, 1000, "col-xs-6 gameBtn yellow");
             break;
@@ -124,7 +157,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-  }
+
 
 
 
